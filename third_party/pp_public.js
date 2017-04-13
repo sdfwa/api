6a9b5df63d56ef78f30835356550fa5ca0019e08
sdfwa <shopit@sdfwa.org>
2017-04-13 08:24:10 -0700

    test

--- third_party/pp_public.js
+++ third_party/pp_public.js
@@ -1,16 +1,3 @@
-/*
-
-<?php
-  $seconds_to_cache = 5 * 60; // 5 minutes
-  $ts = gmdate("D, d M Y H:i:s", time() + $seconds_to_cache) . " GMT";
-  header("Expires: $ts");
-  header("Pragma: cache");
-  header("Cache-Control: max-age=$seconds_to_cache");
-  header('Content-Type: application/javascript');
-?>
-
-*/
-alert('start')
 if(typeof jQuery === 'undefined'){
   (function(s,d,f,w,a){
     a = s.getElementsByTagName('body')[0];
