55863c29af548edd61155528166654df03ea34b4
sdfwa <shopit@sdfwa.org>
2017-04-12 18:34:37 -0700

    ie

--- third_party/pp_public.js
+++ third_party/pp_public.js
@@ -236,8 +236,9 @@ if(typeof jQuery === 'undefined'){
               });
             }, 1000);
           }($, s))
+        }else{
+          s.showModal({type:"text",title:"Welcome to the SDFWA Member Shop!",body:s.tmp.body});
         }
-        s.showModal({type:"text",title:"Welcome to the SDFWA Member Shop!",body:s.tmp.body});
         $('a:contains("Home")').text('Home - Checklist');
       }
     /* end modal code */
