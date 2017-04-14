57748e18b987dffd490ea3fff7d39d57b7690981
sdfwa <shopit@sdfwa.org>
2017-04-13 18:15:38 -0700

    minor updates

--- third_party/pp_public.js
+++ third_party/pp_public.js
@@ -221,11 +221,11 @@ if(typeof jQuery === 'undefined'){
                 <h5>\
                   A good place to start is to make sure you understand that this is your Shop!  It is "of, by and for" our Members.  We want you to not only use the Shop but help us make it better.<br><br>\
                   This checklist will help get you in the shop as soon as possible.  And once you complete the list, we will update our records so you don\'t have to see this popup again.  Please be patient with us while we get our records in order.<br><br>\
-                  All of us will need to go through a basic Shop Safety Training program.  This training is essential to insure that we offer everyone a safe environment, that everyone knows how to operate each piece of equipment safely and to satisfy our insurance provider.\
+                  All of us will need to go through a basic Shop Safety Training program.  This training is essential to ensure that we offer everyone a safe environment, that everyone knows how to operate each piece of equipment safely and to satisfy our insurance provider.\
               ';
               s.tmp.body += '\
                 <h4>\
-                  1) Jump over to <a href="https://asoft10232.accrisoft.com/sdfwa/forms/sdfwa-membership-application-jan-sept/" target="_blank">sdfwa membership</a> site and get a $30 Assoc Membership (Skip step if you\'re already an Assoc Member).\
+                  1) To join the Member Shop, you must be a member of the San Diego Fine Woodworkers Association. Jump over to <a href="https://asoft10232.accrisoft.com/sdfwa/forms/sdfwa-membership-application-jan-sept/" target="_blank">sdfwa membership</a> site and get a $30 Assoc Membership (Skip step if you\'re already an Assoc Member).\
                 </h4>\
               ';
               s.tmp.body += '\
