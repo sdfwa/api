d83d23cbeea8cf8d274b1a940df30ed794f2fb56
Brian Kranson <bkranson@gmail.com>
2017-04-22 17:34:54 -0700

    Test

--- lock/index.php
+++ lock/index.php
@@ -2,6 +2,7 @@
   <html>
   <head>
     <link rel="stylesheet" href="lock.css">
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
     <title>Shop Lock</title>
   </head>
   <body>
