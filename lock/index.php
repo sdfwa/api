2872c5110f3218b9c26efd4a5d0e5b8919d7b50e
Brian Kranson <bkranson@gmail.com>
2017-04-22 17:31:16 -0700

    Test

---
+++ lock/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+  <html>
+  <head>
+    <link rel="stylesheet" href="lock.css">
+    <title>Shop Lock</title>
+  </head>
+  <body>
+    <div id="PINcode"></div>
+    <script type="text/javascript" src="lock.js"></script>
+  </body>
+</html>
