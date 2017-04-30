e71a72178cc7003846e786005bf049f9e8e9ee34
Brian Kranson <bkranson@gmail.com>
2017-04-30 13:33:23 -0700

    images

---
+++ lock/attempts.php
@@ -0,0 +1,16 @@
+<?php
+if(isset($_GET['file']) && isset($_GET['file']) !== ''){
+  $file = $_GET['file'];
+  $image = file_get_contents('/var/shop/motion/'.$file);
+  header('Content-type: image/jpeg;');
+  header("Content-Length: " . strlen($image));
+  echo $image;
+}else{
+  $handle = opendir(dirname(realpath(__FILE__)).'/var/shop/motion/');
+  while($file = readdir($handle)){
+      if($file !== '.' && $file !== '..' && preg_grep('/\.jpg$/i', $file))){
+          echo '<img src="attempts.php/?file='.urlencode($file).'" border="0" /><br>';
+      }
+  } 
+}
+?>
