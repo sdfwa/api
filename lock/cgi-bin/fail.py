1cc0ecbcacb6e17016b13837f52b943e790f1c41
Brian Kranson <bkranson@gmail.com>
2017-04-30 13:22:43 -0700

    updates

--- lock/cgi-bin/fail.py
+++ lock/cgi-bin/fail.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
-import os, sys, time, datetime, urlparse
+import os, sys
+import cgi, cgitb
+print os.environ.get("QUERY_STRING", "No Query String in url")
 try:
   member_id = sys.argv[1]
 except:
   try:
-    member_id = self.request.GET.get('member_id')
+    form = cgi.FieldStorage() 
+    member_id = form.getvalue('member_id')
   except:
     member_id = '0000'
 os.system('curl -F "memberId=' + member_id + '" -F "submit=submit" -F "status=fail" -F "fileToUpload=@/ram/motion.jpg" "https://shop.sdfwa.org/lock/upload.php"')
--- lock/cgi-bin/open.py
+++ lock/cgi-bin/open.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
-import os, sys, time, datetime, urlparse
+import os, sys
+import cgi, cgitb
+print os.environ.get("QUERY_STRING", "No Query String in url")
 try:
   member_id = sys.argv[1]
 except:
   try:
-    member_id = self.request.GET.get('member_id')
+    form = cgi.FieldStorage() 
+    member_id = form.getvalue('member_id')
   except:
     member_id = '0000'
 os.system("gpio -g mode 4 out; gpio -g write 4 1; gpio -g write 4 0;")
