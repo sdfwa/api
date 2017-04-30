d253d1949e30876cc72f514e81962252cc771a65
Brian Kranson <bkranson@gmail.com>
2017-04-30 12:35:11 -0700

    typo

--- lock/upload.php
+++ lock/upload.php
@@ -47,7 +47,7 @@ if(isset($_POST["submit"])) {
 <form action="upload.php" method="post" enctype="multipart/form-data">
     Select image to upload:
     <input type="file" name="fileToUpload" id="fileToUpload">
-    <input type="text" name="memberId" id="memberId"
+    <input type="text" name="memberId" id="memberId">
     <input type="submit" value="Upload Image" name="submit">
 </form>
 
