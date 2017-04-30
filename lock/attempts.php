11836263425177061bd39fa28bc906fff3d5bb68
Brian Kranson <bkranson@gmail.com>
2017-04-30 13:44:32 -0700

    try

--- lock/attempts.php
+++ lock/attempts.php
@@ -7,11 +7,16 @@ if(isset($_GET['file']) && isset($_GET['file']) !== ''){
   echo $image;
 }else{
   $handle = opendir('/var/shop/motion/');
+  $files = array();
   while($file = readdir($handle)){
-      if($file !== '.' && $file !== '..' && preg_match('/\.jpg$/i', $file)){
-          //echo '<img src="attempts.php/?file='.urlencode($file).'" border="0" /><br>';
-          echo '<a href="attempts.php/?file='.urlencode($file).'">'.$file.'</a><br>';
-      }
-  } 
+    if($file !== '.' && $file !== '..' && preg_match('/\.jpg$/i', $file)){
+      array_push($files, $file);
+    }
+  }
+  rsort($files);
+  foreach($files as $file){
+    //echo '<img src="attempts.php/?file='.urlencode($file).'" border="0" /><br>';
+    echo '<a href="attempts.php/?file='.urlencode($file).'">'.$file.'</a><br>';
+  }
 }
 ?>
