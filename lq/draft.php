a0c2963055ddf9071f572f73d7642c17c9a2c8f3
Brian Kranson <bkranson@gmail.com>
2017-05-31 09:58:49 -0700

    try

--- lq/draft.php
+++ lq/draft.php
@@ -5,7 +5,7 @@ $g->write_file = fopen($g->local_out_dir . $g->local_out_file, "w");
 while (($g->row = fgetcsv($g->read_file)) !== false) {
     $id = array_shift($g->row);
     $g->row[2] = str_getcsv($g->row[2], ",");
-    fwrite($g->write_file, json_encode($g->row));
+    fwrite($g->write_file, fputcsv($g->row));
 }
 fclose($g->read_file);
 fclose($g->write_file);
