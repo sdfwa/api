07996090ad48a4d90cfce97db07fce53aa5a5b84
Brian Kranson <bkranson@gmail.com>
2017-05-31 11:14:02 -0700

    lq

--- lq/draft.php
+++ lq/draft.php
@@ -5,7 +5,7 @@ while (($row = fgetcsv($g->in_handle)) !== false) {
   $g->write_contact = false;
   $g->write_event = false;
   $g->write_supplement = false;
-  $results_test = json_decode("[]");
+  $results_debug = json_decode("[]");
   $results_contact = json_decode("[]");
   $results_event = json_decode("[]");
   $results_supplement = json_decode("[]");
@@ -20,11 +20,11 @@ while (($row = fgetcsv($g->in_handle)) !== false) {
   foreach($row as $column){
     if($g->debug){
       $g->write_debug = true;
-      array_push($results_test, trim($column));
+      array_push($results_debug, trim($column));
     }
   }
   if($g->write_debug){
-    fputcsv($g->out_test_handle, $results_test);
+    fputcsv($g->out_debug_handle, $results_debug);
   }
   if($g->write_contact){
     fputcsv($g->out_contact_handle, $results_contact);
