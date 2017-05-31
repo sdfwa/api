6dcd8383b6927c60d75b8ae0031e1d791e440daa
Brian Kranson <bkranson@gmail.com>
2017-05-31 09:14:29 -0700

    start

---
+++ lq/draft.php
@@ -0,0 +1,12 @@
+<?php
+require_once('./helper.php'):
+$g->read_file = fopen($g->local_dir . $g->local_in_file, "r");
+$g->write_file = fopen($g->local_dir . $g->local_out_file, "w");
+while (($g->row = fgetcsv($g->read_file)) !== false) {
+    $id = array_shift($g->row);
+    $g->row[2] = str_getcsv($g->row[2], ",");
+    fwrite($g->write_file, $g->row);
+}
+fclose($g->read_file);
+fclose($g->write_file);
+?>
---
+++ lq/helper.php
@@ -0,0 +1,7 @@
+<?php
+$debug = false;
+$g = json_decode("{}");
+$g->local_in_dir = "/var/github/shop/lq/";
+$g->local_in_file = "20170531_trigger_first_100.csv";
+$g->local_out_dir = "/var/lq/";
+$g->local_out_file = "20170531_trigger_first_100.out";
