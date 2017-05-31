d9f931991bef0cc31f087bf18deb5b2cf844314a
Brian Kranson <bkranson@gmail.com>
2017-05-31 10:11:05 -0700

    lq

--- lq/draft.php
+++ lq/draft.php
@@ -3,7 +3,7 @@ require_once('./helper.php');
 $g->read_file = fopen($g->local_in_dir . $g->local_in_file, "r");
 $g->write_file = fopen($g->local_out_dir . $g->local_out_file, "w");
 while (($row = fgetcsv($g->read_file)) !== false) {
-  $results = json_encode("[]");
+  $results = json_decode("[]");
   foreach($row as $column){
     array_push($results, $column);
   }
