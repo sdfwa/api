af1672de56fb70a1f5ad9993b14b4d5a3ca8e4ee
Brian Kranson <bkranson@gmail.com>
2017-05-31 11:07:23 -0700

    draft

--- lq/after.php
+++ lq/after.php
@@ -1,6 +1,6 @@
 <?php
 fclose($g->in_handle);
-fclose($g->out_test_handle);
+fclose($g->out_debug_handle);
 fclose($g->out_contact_handle);
 fclose($g->out_event_handle);
 fclose($g->out_supplement_handle);
--- lq/draft.php
+++ lq/draft.php
@@ -1,29 +1,39 @@
 <?php
 require_once('./before.php');
 while (($row = fgetcsv($g->in_handle)) !== false) {
+  $g->write_debug = false;
   $g->write_contact = false;
   $g->write_event = false;
   $g->write_supplement = false;
+  $results_test = json_decode("[]");
+  $results_contact = json_decode("[]");
+  $results_event = json_decode("[]");
+  $results_supplement = json_decode("[]");
   if(!$g->have_read_header){
     $g->have_read_header = true;
+    $g->write_debug = true;
     $g->write_contact = true;
     $g->write_event = true;
     $g->write_supplement = true;
     $g->header = getHeader($row);
   }
-  $results = json_decode("[]");
   foreach($row as $column){
-    array_push($results, trim($column));
+    if($g->debug){
+      $g->write_debug = true;
+      array_push($results_test, trim($column));
+    }
   }
-  fputcsv($g->out_test_handle, $results);
+  if($g->write_debug){
+    fputcsv($g->out_test_handle, $results_test);
+  }
   if($g->write_contact){
-    fputcsv($g->out_contact_handle, $results);
+    fputcsv($g->out_contact_handle, $results_contact);
   }
   if($g->write_event){
-    fputcsv($g->out_event_handle, $results);
+    fputcsv($g->out_event_handle, $results_event);
   }
   if($g->write_supplement){
-    fputcsv($g->out_supplement_handle, $results);
+    fputcsv($g->out_supplement_handle, $results_supplement);
   }
 }
 require_once('./after.php');
