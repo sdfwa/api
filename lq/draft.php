4b3db820a0f9a0d55163b1816ffd943398943016
Brian Kranson <bkranson@gmail.com>
2017-05-31 13:18:36 -0700

    lq

--- lq/draft.php
+++ lq/draft.php
@@ -1,5 +1,42 @@
 <?php
-require_once('./before.php');
+/* Start helper funstions */
+function getHeader($row){
+  $results = json_decode("{}");
+  $i = 0;
+  foreach($row as $column){
+    $results->$column = $i;
+    $i++;
+  }
+  return $results;
+}
+/* End helper funstions */
+
+/* Start Config Setup */
+$g = json_decode("{}");
+$g->debug = false;
+$g->in_config_dir = "/var/github/shop/lq/";
+$g->in_dir = "/var/lq/";
+$g->out_dir = "/var/lq/";
+$g->in_file = "20170531_trigger_first_100.csv";
+$g->in_mapping_file = "mapping.json";
+$g->out_debug = "20170531_trigger_first_100_debug.out";
+$g->out_contact = "20170531_trigger_first_100_contact.out";
+$g->out_event = "20170531_trigger_first_100_event.out";
+$g->out_supplement = "20170531_trigger_first_100_supplement.out";
+$g->in_handle = fopen($g->in_dir . $g->in_file, "r");
+$g->out_debug_handle = fopen($g->out_dir . $g->out_debug, "w");
+$g->out_contact_handle = fopen($g->out_dir . $g->out_contact, "w");
+$g->out_event_handle = fopen($g->out_dir . $g->out_event, "w");
+$g->out_supplement_handle = fopen($g->out_dir . $g->out_supplement, "w");
+$g->have_read_header = false;
+$g->read_header = json_decode(file_get_contents($g->in_config_dir . $g->in_mapping_file));
+
+if(isset($_GET["debug"]) && $_GET["debug"] === "true"){
+  $g->debug = true;
+}
+/* End Config Setup */
+
+/* Start processing Rows */
 while (($row = fgetcsv($g->in_handle)) !== false) {
   $g->write_debug = false;
   $g->write_contact = false;
@@ -39,5 +76,18 @@ while (($row = fgetcsv($g->in_handle)) !== false) {
     fputcsv($g->out_supplement_handle, $results_supplement);
   }
 }
-require_once('./after.php');
+/* End processing rows */
+
+/* Start Cleanup and Send */
+fclose($g->in_handle);
+fclose($g->out_debug_handle);
+fclose($g->out_contact_handle);
+fclose($g->out_event_handle);
+fclose($g->out_supplement_handle);
+if($g->debug === true){
+  echo "<pre>";
+  print_r($g);
+  echo "</pre>";
+}
+/* End Cleanup and Send */
 ?>
